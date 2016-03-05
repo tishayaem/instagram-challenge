5b13ad234b291098284e237988c806b99cfc79e0
Constantin <kostiantynkalinin@gmail.com>
2016-03-05 22:27:07 +0000

    User can leave captions

--- spec/features/posts_feature_spec.rb
+++ spec/features/posts_feature_spec.rb
@@ -8,4 +8,26 @@ feature 'posts' do
       expect(page).to have_link 'Add a post'
     end
   end
+
+  context 'posts have been added' do
+    before do
+      Post.create(caption: 'Me and my sweet kitten')
+    end
+
+    scenario 'display posts' do
+      visit '/posts'
+      expect(page).to have_content('Me and my sweet kitten')
+      expect(page).not_to have_content('No posts yet')
+    end
+  end
+  context 'creating posts' do
+    scenario 'prompts user to fill out a form, then displays the new post' do
+      visit '/posts'
+      click_link 'Add a post'
+      fill_in 'Caption', with: 'Me and my sweet kitten'
+      click_button 'Create Post'
+      expect(page).to have_content 'Me and my sweet kitten'
+      expect(current_path).to eq '/posts'
+    end
+  end
 end
